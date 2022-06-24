0a90c599cc4155b5e4b08a09416d43848003c52e
Cam Rogers <camartrogers@gmail.com>
2022-06-24 16:41:04 -0700

    Added CPT taxonomies
    Taxonomies added

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -200,7 +200,207 @@ $labels = array(
 add_action( 'init', 'rmf_register_custom_post_types' );
 
 
-//Flush Permalinks
+//// Add Taxonomies
+function rmf_register_taxonomies() {
+    // Add Vendor Type taxonomy
+    $labels = array(
+        'name'              => _x( 'Vendor Type', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Vendor Type', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Vendor Type' ),
+        'all_items'         => __( 'All Vendor Type' ),
+        'parent_item'       => __( 'Parent Vendor Type' ),
+        'parent_item_colon' => __( 'Parent Vendor Type:' ),
+        'edit_item'         => __( 'Edit Vendor Type' ),
+        'view_item'         => __( 'View Vendor Type' ),
+        'update_item'       => __( 'Update Vendor Type' ),
+        'add_new_item'      => __( 'Add New Vendor Type' ),
+        'new_item_name'     => __( 'New Vendor Type Name' ),
+        'menu_name'         => __( 'Vendor Type' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'vendor-type' ),
+    );
+    register_taxonomy( 'rmf-vendor-type', array( 'rmf-vendor' ), $args );
+
+        // Add Vendor tier taxonomy
+    $labels = array(
+        'name'              => _x( 'Vendor Tier', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Vendor Tier', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Vendor Tier' ),
+        'all_items'         => __( 'All Vendor Tier' ),
+        'parent_item'       => __( 'Parent Vendor Tier' ),
+        'parent_item_colon' => __( 'Parent Vendor Tier:' ),
+        'edit_item'         => __( 'Edit Vendor Tier' ),
+        'view_item'         => __( 'View Vendor Tier' ),
+        'update_item'       => __( 'Update Vendor Tier' ),
+        'add_new_item'      => __( 'Add New Vendor Tier' ),
+        'new_item_name'     => __( 'New Vendor Tier Name' ),
+        'menu_name'         => __( 'Vendor Tier' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'vendor-tier' ),
+    );
+    register_taxonomy( 'rmf-vendor-tier', array( 'rmf-vendor' ), $args );
+
+    // Feature Musicians taxonomy
+    $labels = array(
+        'name'              => _x( 'Featured', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Featured', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Featured' ),
+        'all_items'         => __( 'All Featured' ),
+        'parent_item'       => __( 'Parent Featured' ),
+        'parent_item_colon' => __( 'Parent Featured:' ),
+        'edit_item'         => __( 'Edit Featured' ),
+        'view_item'         => __( 'View Featured' ),
+        'update_item'       => __( 'Update Featured' ),
+        'add_new_item'      => __( 'Add New Featured' ),
+        'new_item_name'     => __( 'New Featured Name' ),
+        'menu_name'         => __( 'Featured' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'featured' ),
+    );
+    register_taxonomy( 'rmf-featured-musician', array( 'rmf-musician' ), $args );
+
+       // Musicians genre taxonomy
+    $labels = array(
+        'name'              => _x( 'Music Genre', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Music Genre', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Music Genre' ),
+        'all_items'         => __( 'All Music Genre' ),
+        'parent_item'       => __( 'Parent Music Genre' ),
+        'parent_item_colon' => __( 'Parent Music Genre:' ),
+        'edit_item'         => __( 'Edit Music Genre' ),
+        'view_item'         => __( 'View Music Genre' ),
+        'update_item'       => __( 'Update Music Genre' ),
+        'add_new_item'      => __( 'Add New Music Genre' ),
+        'new_item_name'     => __( 'New Music Genre Name' ),
+        'menu_name'         => __( 'Music Genre' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'genre' ),
+    );
+    register_taxonomy( 'rmf-music-genre', array( 'rmf-musician' ), $args );
+       // Workshop Type taxonomy
+    $labels = array(
+        'name'              => _x( 'Workshop Type', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Workshop Type', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Workshop Type' ),
+        'all_items'         => __( 'All Workshop Type' ),
+        'parent_item'       => __( 'Parent Workshop Type' ),
+        'parent_item_colon' => __( 'Parent Workshop Type:' ),
+        'edit_item'         => __( 'Edit Workshop Type' ),
+        'view_item'         => __( 'View Workshop Type' ),
+        'update_item'       => __( 'Update Workshop Type' ),
+        'add_new_item'      => __( 'Add New Workshop Type' ),
+        'new_item_name'     => __( 'New Music Workshop Type' ),
+        'menu_name'         => __( 'Workshop Type' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'workshop-type' ),
+    );
+    register_taxonomy( 'rmf-workshop-type', array( 'rmf-workshop' ), $args );    
+       // FAQ Category taxonomy
+    $labels = array(
+        'name'              => _x( 'FAQ Category', 'taxonomy general name' ),
+        'singular_name'     => _x( 'FAQ Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search FAQ Category' ),
+        'all_items'         => __( 'All FAQ Category' ),
+        'parent_item'       => __( 'Parent FAQ Category' ),
+        'parent_item_colon' => __( 'Parent FAQ Category:' ),
+        'edit_item'         => __( 'Edit FAQ Category' ),
+        'view_item'         => __( 'View FAQ Category' ),
+        'update_item'       => __( 'Update FAQ Category' ),
+        'add_new_item'      => __( 'Add New FAQ Category' ),
+        'new_item_name'     => __( 'New Music FAQ Category' ),
+        'menu_name'         => __( 'FAQ Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'faq-category' ),
+    );
+    register_taxonomy( 'rmf-category-type', array( 'rmf-faq' ), $args );    
+           // Age Group taxonomy
+    $labels = array(
+        'name'              => _x( 'Age Group', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Age Group', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Age Group' ),
+        'all_items'         => __( 'All Age Group' ),
+        'parent_item'       => __( 'Parent Age Group' ),
+        'parent_item_colon' => __( 'Parent Age Group:' ),
+        'edit_item'         => __( 'Edit Age Group' ),
+        'view_item'         => __( 'View Age Group' ),
+        'update_item'       => __( 'Update Age Group' ),
+        'add_new_item'      => __( 'Add New Age Group' ),
+        'new_item_name'     => __( 'New Music Age Group' ),
+        'menu_name'         => __( 'Age Group' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'age-group' ),
+    );
+    register_taxonomy( 'rmf-age-group', array( 'rmf-workshop', 'rmf-musician' ), $args );    
+
+}
+add_action( 'init', 'rmf_register_taxonomies');
+
+
+
+///Flush Permalinks///
 function crog_rewrite_flush() {
     c_rog_register_custom_post_types();
     flush_rewrite_rules();
