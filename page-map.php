092d08b7dd9db10f32b1e82e83120e93819a9216
austyn6 <austyn.philpott@gmail.com>
2022-06-27 13:09:22 -0700

    file cleanup

--- page-map.php
+++ page-map.php
@@ -45,25 +45,6 @@ get_header();
             endif;
             ?>
         </section>
-        <?php
-
-        // Check rows exists.
-        if( have_rows('repeater_field_name') ):
-
-            // Loop through rows.
-            while( have_rows('repeater_field_name') ) : the_row();
-
-                // Load sub field value.
-                $sub_value = get_sub_field('sub_field');
-                // Do something...
-
-            // End loop.
-            endwhile;
-
-        // No value.
-        else :
-            // Do something...
-        endif; ?>
 	</main><!-- #main -->
 
 <?php
