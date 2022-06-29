eebc9c810d22defc74afd36f3de15c9c0a1f52ad
Jayson Postle <jaysondigitalmcc@gmail.com>
2022-06-29 14:22:00 -0700

    jonathan edits: semantic html, wp attachment etc

--- archive-rmf-faq.php
+++ archive-rmf-faq.php
@@ -16,36 +16,18 @@ get_header();
 
 			<header class="page-header">
 				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				post_type_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
-			// while ( have_posts() ) :
-			// 	the_post();
-
-			// 	/*
-			// 	 * Include the Post-Type-specific template for the content.
-			// 	 * If you want to override this in a child theme, then include a file
-			// 	 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-			// 	 */
-			// 	get_template_part( 'template-parts/content', get_post_type() );
-
-			// endwhile;
-
-			// the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
+			
 
 		endif;
 		?>
 
         <section>
-			<h2>FAQ</h2>
             <div>
                 <p>Sort content goes here. Filter by taxonomy (general, admission, camping, etc)</p>
             </div>
@@ -100,5 +82,4 @@ get_header();
 	</main><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
