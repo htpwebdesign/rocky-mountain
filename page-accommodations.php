ec51045b62be18a0f4eea8bb4c4fdd8017a8ecf0
Cam Rogers <camartrogers@gmail.com>
2022-07-06 15:15:31 -0700

    Accordions partially styled
    Still need accordion illustration applied

--- page-accommodations.php
+++ page-accommodations.php
@@ -21,13 +21,13 @@ get_header();
 		while ( have_posts() ) :
 			the_post();
 		?>
-		<section>
+		<section class='camping'>
 			<button class="accordion"><h2><?php the_field('camping_title') ?></h2></button>
 			<div class="panel">
 				<p><?php the_field('camping_description') ?></p>
 			</div>
 		</section>
-		<section>
+		<section class='alt-accom'>
 			<button class="accordion"><h2><?php the_field('accomm_title') ?></h2></button>
 			<div class="panel">
 				<p><?php the_field('accomm_description') ?></p>
@@ -36,15 +36,19 @@ get_header();
 					// Loop through rows.
 					while( have_rows('nearby_accomm') ) : the_row();
 					?>
-					<article>
-						<h3><a href="<?php the_sub_field('accom_link') ?> "><?php the_sub_field('name') ?></a></h3>
-						<address><?php the_sub_field('address') ?></address>
-						<p><a href="tel: <?php the_sub_field('phone_number')?>"><?php the_sub_field('phone_number')?></a></p>
+					<article class='hotel'>
+						<div>
+							<h3><a href="<?php the_sub_field('accom_link') ?> "><?php the_sub_field('name') ?></a></h3>
+							<div class='hotel-contact'>
+								<address><?php the_sub_field('address') ?></address>
+								<p><a href="tel: <?php the_sub_field('phone_number')?>"><?php the_sub_field('phone_number')?></a></p>
+							</div>
+						</div>
 						<figure>
 							<?php $image= get_sub_field('image');  
 							if ($image) {
 								echo wp_get_attachment_image( $image['id'], 'large');
-							} ?>" />
+							} ?>
 						</figure>
 
 					</article>
