030a000240d0709da9726b906a58fa3c7a496cdd
Mike Yamauchi <mikes.yamauchi@gmail.com>
2022-07-09 14:37:04 -0700

    sass updated

--- archive-rmf-musician.php
+++ archive-rmf-musician.php
@@ -46,6 +46,7 @@ get_header();
         the_archive_description( '<div>', '</div>')
         ?>
     </header>
+    <div class="archive-cards">
     <?php
     $args = array(
         'post_type' => 'rmf-musician',
@@ -58,32 +59,37 @@ get_header();
     if ( $query->have_posts() ) {
         while ( $query->have_posts() ) {
             $query->the_post();
-
-            echo '<article class="grid-item '.isotope_musician_classes(get_the_id()).'">';
+            echo '<article class="grid-item  archive-line-up '.isotope_musician_classes(get_the_id()).'">';
                 echo '<h2>'. get_the_title() .'</h2>';
                 the_post_thumbnail( 'large' );
+
                 get_categories();
                 if ( function_exists ( 'get_field' ) ) {
-			
+                        
                     if ( get_field( 'band_description' ) ) {
                         echo '<p>'. the_field( 'band_description' ) .'</p>';
                     }
                     if ( get_field( 'day' ) ) {
                         echo '<p>'. the_field( 'day' ) .'</p>';
                     }
-                    if ( get_field( 'start_time' ) ) {
-                        echo '<p>'. the_field( 'start_time' ) .'</p>';
-                    }
+                    if ( get_field( 'location' ) ) {
+                        echo '<p>'. the_field('location') .'</p>'; 
+                        $startTime = explode('2022', get_field('start_time')); 
+                        echo $startTime[1], ' -';
+                        $endTime = explode('2022', get_field('end_time')); 
+                        echo $endTime[1]; 
+                    }    
                 };
 
 
                 the_excerpt();
             echo '</article>';
-
+            
         }
         wp_reset_postdata();
     }
     ?>
+    </div>
 </main>
 </div>
 <?php
