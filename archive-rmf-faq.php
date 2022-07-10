275f3d4969dd494baa7762f521e7c959503b60c3
austyn6 <austyn.philpott@gmail.com>
2022-07-09 19:07:03 -0700

    Styles & Fixes - FAQ, Lineup, Workshops, Vendors
    Styled filter menu for FAQ, Lineup, Workshop and Vendors page. Cleaned up code in respective PHP files (removed squirrelly brackets and replaced with endifs; etc. Tidied up code, fixed some HTML semantics)

--- archive-rmf-faq.php
+++ archive-rmf-faq.php
@@ -8,34 +8,29 @@
  */
 
 get_header();
-get_template_part('template-parts/content-banner');
-?>
+get_template_part('template-parts/content-banner'); ?>
 
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<?php
-
-		endif;
-		?>
-        <section>
-			<div class="button-group filter-button-group">
-				<button data-filter="*">Show All</button>
-				<?php
+	<main id="primary" class="site-main"> <?php 
+		
+		if ( have_posts() ) : 
+		endif; ?>
+        
+		<div class="filter-wrapper">
+			<section class="button-group filter-button-group">
+				<button data-filter="*">Show All</button> <?php
+				
 				$terms = get_terms( array(
 					'taxonomy' => 'rmf-category-type',
 					'hide_empty' => false,
 				));
-				foreach($terms as $term) {
+				
+				foreach($terms as $term) :
 					echo '<button data-filter=".'.$term->slug.'">'.$term->name.'</button>';
-				}
-			?>
-			</div>
-        </section>
+				endforeach; ?>
+			</section>
+		</div>
 
-        <section class="grid">
-		<?php
+        <section class="grid"> <?php
 			$args = array( 
 				// matches with unique name we gave the cpt
 				'post_type' => 'rmf-faq',
@@ -43,29 +38,23 @@ get_template_part('template-parts/content-banner');
 			);
 
 			$query = new WP_Query( $args );
-			if ($query -> have_posts()) {
-				
-				while ( $query -> have_posts()) {
-					$query -> the_post();
-					echo '<article class="grid-item '.isotope_faq_classes(get_the_id()).'">';
-					?>
-	
-					<button class="accordion"><?php the_title(); ?></button>
-					<div class="panel">
 
-					<?php
-					the_field('faq_description');
-					?>
-					</div>
-					<?php
-					echo '</article>';
+			echo '<section class="faq-layout">';
 
-				}
-				?>
-				<?php
-				wp_reset_postdata();
-			}
-		?>
+				if ($query -> have_posts()) :
+					
+					while ( $query -> have_posts()) :
+						$query -> the_post();
+						echo '<article class="grid-item '.isotope_faq_classes(get_the_id()).'">'; ?>
+						<button class="accordion"><?php the_title(); ?></button>
+							<div class="panel">
+								<?php the_field('faq_description'); ?>
+							</div> <?php
+						echo '</article>';
+					endwhile;
+					wp_reset_postdata();
+				endif; ?>
+			</section>
 		</section>
 
 	</main><!-- #primary -->
--- archive-rmf-workshop.php
+++ archive-rmf-workshop.php
@@ -8,69 +8,77 @@
  */
 
 get_header();
-get_template_part('template-parts/content-banner');
-?>
-    <div class="button-group filter-button-group">
-        <button data-filter="*">Show All</button>
-        <?php
+get_template_part('template-parts/content-banner');?>
 
+<div class="filter-wrapper">
+    <section class="button-group filter-button-group">
+        <button data-filter="*">Show All</button> <?php
+        
         $terms = get_terms( array(
             'taxonomy' => 'rmf-workshop-type',
             'hide_empty' => false,
         ));
-        foreach($terms as $term) {
+        
+        foreach($terms as $term) :
             echo '<button data-filter=".'.$term->slug.'">'.$term->name.'</button>';
-        }
+        endforeach;
 
         $terms = get_terms( array(
             'taxonomy' => 'rmf-age-group',
             'hide_empty' => false,
         ));
-        foreach($terms as $term) {
+
+        foreach($terms as $term) :
             echo '<button data-filter=".'.$term->slug.'">'.$term->name.'</button>';
-        }
-        ?>
+        endforeach;?>
 
-    </div>
+    </section>
+</div>
+
 <div class="grid">
-<main>
-    <header>
-        <?php
-        post_type_archive_title( '<h1>', '</h1>' );
-        the_archive_description( '<div>', '</div>')
-        ?>
-    </header>
+    <main>
+        <header> <?php
+            post_type_archive_title( '<h1>', '</h1>' );
+            the_archive_description( '<div>', '</div>'); ?>
+        </header> <?php
 
-    <?php
-    $args = array(
-        'post_type' => 'rmf-workshop',
+        $args = array(
+            'post_type' => 'rmf-workshop',
+            'posts_per_page' => -1
+        );
 
-        'posts_per_page' => -1
-    );
+        $query = new WP_Query( $args );
+        
+        echo '<section class="workshop-layout">';
+            
+            if ( $query->have_posts() ) :
+                while ( $query->have_posts() ) :
+                    $query->the_post();
 
-    $query = new WP_Query( $args );
+                    echo '<article class="grid-item '.isotope_workshop_classes(get_the_id()).'">';
+                        echo '<h2>'. get_the_title() .'</h2>';
+                        the_post_thumbnail( 'large' );
+                        
+                        if ( function_exists ( 'get_field' ) ) :                
+                            if ( get_field( 'workshop_description' ) ) :
+                                echo '<p>'. the_field( 'workshop_description' ) .'</p>';
+                            endif;
+                            if ( get_field( 'day' ) ) :
+                                echo '<p>'. the_field( 'day' ) .'</p>';
+                            endif;
+                            if ( get_field( 'start_time' ) ) :
+                                echo '<p>'. the_field( 'start_time' ) .'</p>';
+                            endif;
+                        endif;
+                        
+                        the_excerpt();
+                    echo '</article>';
 
-    if ( $query->have_posts() ) {
-        while ( $query->have_posts() ) {
-            $query->the_post();
-
-            echo '<article class="grid-item '.isotope_workshop_classes(get_the_id()).'">';
-                echo '<h2>'. get_the_title() .'</h2>';
-                the_post_thumbnail( 'large' );
-                if ( function_exists ( 'get_field' ) ) {
-			
-                    if ( get_field( 'workshop_description' ) ) {
-                        echo '<p>'. the_field( 'workshop_description' ) .'</p>';
-                    }
-                };
-
-            echo '</article>';
-
-        }
-        wp_reset_postdata();
-    }
-    ?>
-</main>
+                endwhile;
+                wp_reset_postdata();
+            endif;
+        echo '</section>'; ?>
+    </main>
 </div>
 <?php
 get_footer();
