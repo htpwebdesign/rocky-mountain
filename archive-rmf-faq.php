732c34c4e7e8d225102a477de7fe907683dc0eea
Mike Yamauchi <mikes.yamauchi@gmail.com>
2022-07-13 15:00:54 -0700

    FAQ styled, Isotope dropped from page!

--- archive-rmf-faq.php
+++ archive-rmf-faq.php
@@ -13,49 +13,39 @@ get_template_part('template-parts/content-banner'); ?>
 	<main id="primary" class="site-main"> <?php 
 		
 		if ( have_posts() ) : 
-		endif; ?>
-        <!-- <div class="main-interior-wrapper"> -->
-		<!-- <div class="filter-wrapper">
-			<section class="button-group filter-button-group">
-				<button data-filter="*">Show All</button> <?php
-				
-				$terms = get_terms( array(
-					'taxonomy' => 'rmf-category-type',
-					'hide_empty' => false,
-				));
-				
-				foreach($terms as $term) :
-					echo '<button data-filter=".'.$term->slug.'">'.$term->name.'</button>';
-				endforeach; ?>
-			</section>
-		</div> -->
-        <div class="main-interior-wrapper">
+		endif; 				
+			?>
+			<div class="faq-beige-box">
+			<div class="faq-green-box">
+				<section class=" faq-wrapper">
+ <?php
+					$args = array( 
+						// matches with unique name we gave the cpt
+						'post_type' => 'rmf-faq',
+						'posts_per_page' => -1
+					);
 
-        <section class=" faq-wrapper grid-wrapper"> <?php
-			$args = array( 
-				// matches with unique name we gave the cpt
-				'post_type' => 'rmf-faq',
-				'posts_per_page' => -1
-			);
-
-			$query = new WP_Query( $args );
+					$query = new WP_Query( $args );
 
 
-				if ($query -> have_posts()) :
-					
-					while ( $query -> have_posts()) :
-						$query -> the_post();
-						echo '<article class="faq-item  '.isotope_faq_classes(get_the_id()).'">'; ?>
-						<button class="accordion"><?php the_title(); ?></button>
-							<div class="panel">
-								<?php the_field('faq_description'); ?>
-							</div> <?php
-						echo '</article>';
-					endwhile;
-					wp_reset_postdata();
-				endif; ?>
+						if ($query -> have_posts()) :
+							
+							while ( $query -> have_posts()) :
+								$query -> the_post();
+								?><div class="faq-even-odd"><?php
+								echo '<article class="faq-article">'; ?>
+								<button class="accordion"><?php the_title(); ?></button>
+									<div class="panel">
+										<?php the_field('faq_description'); ?>
+									</div> <?php
+								echo '</article>';
+								echo '</div>';
+							endwhile;
+							wp_reset_postdata();
+						endif; ?>
 
-		</section>
+				</section>
+			</div>
 			</div>
 	</main><!-- #primary -->
 
